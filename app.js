820839eb1de87970147d1d8251404e02d51a01df
myemets <mykola.yemets@macys.com>
2024-04-25 19:52:17 +0200

    Nest setup

--- app.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-http.createServer(function(req, res) {
- res.write("Second touch");
- res.end();
-}).listen(3000);
-
-console.log("Started server on port 3000 new");
